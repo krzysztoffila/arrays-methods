652405fb96a4ce0e4425530682e5bee5a181e73d
krzysztoffila <umowy.fila@gmail.com>
2023-06-13 07:25:42 +0200

    finish in PL

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-/* values(): This method returns an iterator that provides the values for each index in the array. It takes no arguments. */
+/* values(): Ta metoda zwraca iterator, który dostarcza wartości dla każdego indeksu w tablicy. Nie przyjmuje żadnych argumentów. */
 const arr = ['apple', 'banana', 'cherry'];
 const iterator = arr.values()
 
@@ -6,30 +6,30 @@ for (const value of iterator) {
     console.log.apply(value);
 } // Output: apple banana cherry
 
-/* length(): This property returns th length of array. */
+/* length(): Ta właściwość zwraca długość tablicy. */
 const arrLength = ['apple', 'banana', 'cherry'];
 console.log(arrLength.length); // Output: 3
 
-/* reverse(): This method reverses the order of the elements in the array. */
+/* reverse(): Ta metoda odwraca kolejność elementów w tablicy. */
 const arrReverse = ['apple', 'banana', 'cherry'];
 arrReverse.reverse();
 console.log(arrReverse) // Output: ['cherry', 'banana', 'apple'];
 
-/* sort(): This method sorts the elements of an array in place and returns the sorted array. It can take an optional compare function as an argument. */
+/* sort(): Ta metoda sortuje elementy tablicy w miejscu i zwraca posortowaną tablicę. Jako argument może przyjąć opcjonalną funkcję porównania. */
 const arrSorted = ['banana', 'apple', 'cherry'];
 arrSorted.sort();
 console.log(arrSorted) // Output: ['apple', 'banana', 'cherry'];
 
-/* at(): This metgod returns the element at the specified index in the array. It takes one argument: the index. */
+/* at(): Ta metoda zwraca element na podanym indeksie w tablicy. Przyjmuje jeden argument: indeks. */
 const arrElement = ['banana', 'apple', 'cherry'];
 console.log(arrElement.at(1)) // Output: 'apple'
 
-/* fill(): This method fills all the elements of an array from a start index to an end index with a static value. It can take up to three arguments: the value to fill with, the start index, and the end index. */
+/* fill(): Ta metoda wypełnia wszystkie elementy tablicy od indeksu początkowego do indeksu końcowego wartością statyczną. Może przyjmować do trzech argumentów: wartość do wypełnienia, indeks początkowy i indeks końcowy. */
 const arrFill = ['apple', 'banana', 'cherry'];
 arrFill.fill('orange', 1, 2);
 console.log(arrFill) // Output ['apple', 'orange', 'cherry'];
 
-/* from(): This method creates a new array from an array-like object. It cna take up to two arguments: the object to convert to an array, and a mapping function to apply to each element of the new array. */
+/* from(): Ta metoda tworzy nową tablicę z obiektu podobnego do tablicy. Może przyjmować do dwóch argumentów: obiekt do konwersji na tablicę i funkcję mapowania do zastosowania do każdego elementu nowej tablicy. */
 const obj = {
     0: 'apple',
     1: 'banana',
@@ -38,108 +38,108 @@ const obj = {
 const arrFromObj = Array.from(obj);
 console.log(arrFromObj); // Output: ['apple', 'banana', 'cherry'];
 
-/* join(): This method joins all the elements of an array into a string using a specified separator. It takes one optional argument: the separator to use. */
+/* join(): Ta metoda łączy wszystkie elementy tablicy w ciąg znaków przy użyciu określonego separatora. Przyjmuje jeden opcjonalny argument: separator do użycia. */
 const arrToJoined = ['apple', 'banana', 'cherry'];
 const arrJoined = arrToJoined.join(' or ');
 console.log(arrJoined) // Output: 'apple or banana or cherry';
 
-/* toString(): This method returns a string representing the array and its elements. */
+/* toString(): Ta metoda zwraca ciąg znaków reprezentujący tablicę i jej elementy. */
 const arrToString = ['apple', 'banana', 'cherry'];
 const str = arrToString.toString();
 console.log(str) // Output: 'apple,banana,cherry'
 
-/* pop(): This method removes the last element from an arrau and returns that element. */
+/* pop(): Ta metoda usuwa ostatni element z tablicy i zwraca ten element. */
 const arrFull = ['apple', 'banana', 'cherry'];
 const last = arrFull.pop();
 console.log(last); // Output: 'cherry'
 console.log(arrFull); // Output: ['apple', 'banana'];
 
-/* forEach(): This method executes a provided function once for each array element. It doesn't return anything, it just executes the callback function on each element of the array */
+/* forEach(): Ta metoda wykonuje dostarczoną funkcję raz dla każdego elementu tablicy. Nic nie zwraca, po prostu wykonuje funkcję zwrotną na każdym elemencie tablicy */
 let fruits = ['apple', 'banana', 'cherry'];
 fruits.forEach(function (item) {
     console.log(item);
 }); // Output: apple, banana, cherry;
 
-/* shift(): This method removes the first element of an array and returns that removed element. This metgod changes the length of the array. */
+/* shift(): Metoda ta usuwa pierwszy element tablicy i zwraca usunięty element. Metoda ta zmienia długość tablicy. */
 let fruitsAll = ['apple', 'banana', 'cherry'];
 let shiftFruit = fruits.shift();
 console.log(shiftFruit); // Output: 'apple'
 console.log(fruitsAll); // Output: ['banana', 'cherry];
 
-/* copyWithin(): This method shallow copies part of an array to another location in the same array and returns the modified array withoud modifying its length.Syntax.copyWithin(target, start, end) */
+/* copyWithin(): Ta metoda płytko kopiuje część tablicy do innego miejsca w tej samej tablicy i zwraca zmodyfikowaną tablicę bez modyfikowania jej długości.Składnia.copyWithin(target, start, end) */
 let numbers = [1, 2, 3, 4, 5];
 numbers.copyWithin(2, 0, 2);
 console.log(numbers); // Output: [1, 2, 1, 2, 5];
 
-/* push(): This method adds one or more elements to the end of an arrat and returns the new length of the array. */
+/* push(): Ta metoda dodaje jeden lub więcej elementów na koniec tablicy i zwraca nową długość tablicy. */
 let moreFruits = ['apple', 'banana'];
 moreFruits.push('cherry', 'orange');
 console.log(moreFruits); // Output: ['apple', 'banana', 'cherry', 'orange'];
 
-/* unshift(): This method adds one or more elements to the beginning of an array and returns the new length of the array. */
+/* unshift(): Ta metoda dodaje jeden lub więcej elementów na początku tablicy i zwraca nową długość tablicy. */
 let againFruits = ['cherry', 'orange'];
 againFruits.unshift('apple', 'banana');
 console.log(againFruits) // Output: ['apple', 'banana', 'cherry', 'orange'];
 
-/* concat(): This method is used to merge two or more arratys. This metghod does not change the existing arrays, but instead returns a new array. */
+/* concat(): Ta metoda służy do łączenia dwóch lub więcej tablic. Metoda ta nie zmienia istniejących tablic, ale zwraca nową tablicę. */
 let andAgainFruits = ['apple', 'banana'];
 let andMoreFruits = ['cherry', 'orange'];
 let allOfThem = andAgainFruits.concat(andMoreFruits);
 console.log(allOfThem); // Output: ['apple', 'banana', 'cherry', 'orange'];
 
-/* splice(): This method changes the contents of an array by removing or replacimg existing elements and/or adding new elements in place. */
+/* splice(): Metoda ta zmienia zawartość tablicy poprzez usunięcie lub zastąpienie istniejących elementów i/lub dodanie nowych elementów w ich miejsce. */
 const fruitsToSplice = ['apple', 'banana', 'cherry', 'orange'];
 //Syntax : arr.splice(start, deleteCount, item1, ..., itemN)
 fruitsToSplice.splice(2, 1, 'mango', 'kiwi');
 console.log(fruitsToSplice); // // Output: ['apple', 'banana', 'mango', 'kiwi', 'cherry', 'orange'];
 
-/* flat(): This method creates an new array with all sub-array elements concatenated into it recursively up the specified depth. */
+/* flat(): Ta metoda tworzy nową tablicę ze wszystkimi elementami podtablicy konkatenowanymi w niej rekurencyjnie do określonej głębokości. */
 const numbersNoFlat = [1, [2, [3]], 4];
 const flatNumbers = numbersNoFlat.flat(Infinity);
 console.log(flatNumbers); // Output: [1, 2, 3, 4];
 
-/* lastIndexOf(): This method returns the last index at which a given element can be found in the array. */
+/* lastIndexOf(): Ta metoda zwraca ostatni indeks, przy którym dany element można znaleźć w tablicy. */
 const numbersToSearch = [1, 2, 3, 4, 5, 3];
 const lastIndex = numbers.lastIndexOf(3);
 console.log(lastIndex); // Output: 5;
 
-/* indexOf(): This method returns the index of the occurrence of a specified element in an array. If the elements is not present, it returns -1. */
+/* indexOf(): Ta metoda zwraca indeks wystąpienia określonego elementu w tablicy. Jeśli element nie występuje, zwraca -1. */
 const moreNumbers = [5, 10, 15, 20];
 const index = moreNumbers.indexOf(10);
 console.log(index); // Output: 1;
 
-/* of(): this method creates a new array instance with a variable number of arguments, reagardless of number or type the arguments. */
+/* of(): ta metoda tworzy nową instancję tablicy ze zmienną liczbą argumentów, niezależnie od liczby lub typu argumentów. */
 const mixArr = Array.of(1, 2, 3, "four", true);
 console.log(mixArr); // Output: [1, 2, 3, "four", true];
 
-/* every(): This method checks if all elements in an array pass a test(provided as a function). It return true if all elements pass the test; otherwise, it returns false */
+/* every(): Ta metoda sprawdza, czy wszystkie elementy w tablicy przechodzą test (podany jako funkcja). Zwraca wartość true, jeśli wszystkie elementy przejdą test; w przeciwnym razie zwraca wartość false */
 const evenArr = [2, 4, 6, 8];
 const isEven = (num) => num % 2 === 0;
 const result = evenArr.every(isEven);
 console.log(result); // Output: true;
 
-/* slice(): This method returns a shallow copy of a portion of an array into a new array object selected from begin to end(end not included). */
+/* slice(): Ta metoda zwraca płytką kopię części tablicy do nowego obiektu tablicy wybranego od początku do końca (koniec nie jest uwzględniony) */
 const arrToSliced = [1, 2, 3, 4, 5];
 const slicedArr = arrToSliced.slice(2, 4);
 console.log(slicedArr); // Output: [3, 4];
 
-/* flatMap(): This method maps each element using a mapping function, then flattens the result into a new array. */
+/* flatMap(): Ta metoda mapuje każdy element za pomocą funkcji mapującej, a następnie spłaszcza wynik do nowej tablicy. */
 const arrToFlat = [1, 2, 3];
 const resultFlat = arrToFlat.flatMap(x => [x * 2]);
 console.log(resultFlat); // Output [2, 4, 6];
 
-/* findIndex(): This metgod returns the index of the firs element in an array that passes a test(provided as a funstion). If no element pases the test , it returns -1. */
+/* findIndex(): Ta metoda zwraca indeks pierwszego elementu w tablicy, który przeszedł test (dostarczony jako funkcja). Jeśli żaden element nie przejdzie testu, zwraca -1. */
 const highArr = [10, 20, 30, 40, 50];
 const greaterThan30 = (num) => num > 30;
 const giveMeTheIndex = arr.findIndex(greaterThan30);
 console.log(giveMeTheIndex); // Output: 3;
 
-/* find(): This method returns the value of the first element in an array that passes a test(provided as a function). If no element passes the test, it returns undefined */
+/* find(): Ta metoda zwraca wartość pierwszego elementu w tablicy, który przejdzie test (podany jako funkcja). Jeśli żaden element nie przejdzie testu, zwraca wartość undefined */
 const againHighArr = [10, 20, 30, 40, 50];
 const showResult = arr.find(greaterThan30);
 console.log(showResult); // Output: 40;
 
-/* includes(): This method determines whether an array includes a certaind value among its entires, returning true or false as appropriate. */
+/* includes(): Ta metoda określa, czy tablica zawiera określoną wartość wśród swoich wpisów, zwracając odpowiednio wartość true lub false. */
 const someNumbers = [10, 20, 30, 40, 50];
 const has20 = someNumbers.includes(20);
 console.log(has20); // Output: true;
@@ -151,26 +151,26 @@ console.log(iteratorEntries.next().value); // Output: [0, "a"];
 console.log(iteratorEntries.next().value); // Output: [1, "b"];
 console.log(iteratorEntries.next().value); // Output: [2, "c"];
 
-/* reduce(): this metgod applies a function to each element of an array and reduces the array to a single value. */
+/* reduce(): ta metoda stosuje funkcję do każdego elementu tablicy i redukuje tablicę do pojedynczej wartości. */
 const denominations = [10, 20, 10, 20, 5, 5, 5, 5, 10, 10];
-// accumulator: It is the value returned from previous iteration of the function.
-// curretnValue: It is the current element being processed in the array.
+// accumulator: Jest to wartość zwrócona z poprzedniej iteracji funkcji.
+// curretnValue: Jest to bieżący element przetwarzany w tablicy.
 const sum = numbers.reduce((accumulator, currentValue) => {
     return accumulator + currentValue;
 });
 console.log(sum) // Output: 100;
 
-/* isArray(): This method deterines whether the passed value is an array or not. */
+/* isArray(): Ta metoda określa, czy przekazana wartość jest tablicą, czy nie. */
 const number = 123;
 console.log(Array.isArray(number)) // Output: false;
 
-/* filter(): This method creates a new array with all elements that pass the test implemented by the provided function. */
+/* filter(): Metoda ta tworzy nową tablicę zawierającą wszystkie elementy, które przeszły test zaimplementowany przez podaną funkcję. */
 const moneyArr = [10, 20, 20, 50, 100, 200];
 const filteredMoney = moneyArr.filter(number => {
     return number > 20;
 }); // Output: [50, 100, 200];
 
-/* keys(): This method returns an array containing the kesy of the given object. */
+/* keys(): Metoda ta zwraca tablicę zawierającą klucze danego obiektu. */
 const myObj = {
     a: 1,
     b: 2,
@@ -179,7 +179,7 @@ const myObj = {
 const keysArray = Object.keys(myObj);
 console.log(keysArray); // Output ["a", "b", "c"];
 
-/* map(): This method creates a new array with the results of callung a provided function on every element in the callung array. */
+/* map(): Ta metoda tworzy nową tablicę z wynikami wywołania podanej funkcji na każdym elemencie w wywołanej tablicy. */
 const numbersToDo = [1, 2, 3, 4, 5];
 const squaredNumbers = numbersToDo.map((number) => {
     return number * number
